38dc2bdb7ce5e8443ba8662ffc6190e2b0c0dbb8
turbokwaka <taranenko.artem.work@proton.me>
2026-03-20 15:54:55 +0200

    god help me4

--- modules/load_django.py
+++ modules/load_django.py
@@ -1,8 +1,16 @@
 import os
 import sys
 import django
+from django.apps import apps
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'freelancehunt_project')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'freelancehunt_project.settings'
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'freelancehunt_project'))
+if project_path not in sys.path:
+    sys.path.append(project_path)
 
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelancehunt_project.settings')
+
+if not apps.ready:
+    try:
+        django.setup()
+    except RuntimeError:
+        pass
